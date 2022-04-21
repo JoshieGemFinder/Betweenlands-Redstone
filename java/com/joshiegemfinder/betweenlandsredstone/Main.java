e6d02b36ff447df13414cdfdc6e3cadbe2ef9ecc
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2022-04-21 20:17:48 +1000

    Update Main.java

--- java/com/joshiegemfinder/betweenlandsredstone/Main.java
+++ java/com/joshiegemfinder/betweenlandsredstone/Main.java
@@ -3,6 +3,6 @@ package com.joshiegemfinder.betweenlandsredstone;
 public class Main
 {
   public static void main(String[] args) {
-    System.out.println("Hah! Bet you were expecting the Betweenlands Redstone source code here! I'm still cleaning it up, come back in a two to three business days.")
+    System.out.println("Hah! Bet you were expecting the Betweenlands Redstone source code here! I'm still cleaning it up, come back in two to three business days.")
   }
 }
