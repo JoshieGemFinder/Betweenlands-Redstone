8a49d64ee414f36257f43324232d6d28c94bbaff
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2022-08-05 20:19:04 +1000

    Delete java directory
    im finally getting around to putting stuff here

--- java/com/joshiegemfinder/betweenlandsredstone/Main.java
+++
@@ -1,8 +0,0 @@
-package com.joshiegemfinder.betweenlandsredstone;
-
-public class Main
-{
-  public static void main(String[] args) {
-    System.out.println("Hah! Bet you were expecting the Betweenlands Redstone source code here! I'm still cleaning it up, come back in two to three business days.")
-  }
-}
